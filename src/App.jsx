068f05e97ce80e48f3510d70d180ad057a710e6b
Jose Antonio <confi15826s@gmail.com>
2023-12-28 18:30:01 -0600

    finished project with LS

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
 import { Toaster } from 'sonner';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  
-  const [pacientes, setPacientes] = useState([]);
+
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
   const [paciente, setPaciente] = useState({});
 
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  }, [pacientes])
+
   const eliminarPaciente = id => {
-    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
 
     setPacientes(pacientesActualizados)
   }
@@ -20,19 +24,19 @@ function App() {
     <div className='container mx-auto mt-14'>
       <Header />
       <div className='mt-8 md:flex'>
-      <Formulario
-        pacientes={pacientes}
-        setPacientes={setPacientes}
-        paciente={paciente}
-        setPaciente={setPaciente}
-      />
-      <ListadoPacientes 
-        pacientes={pacientes}
-        setPaciente={setPaciente}
-        eliminarPaciente={eliminarPaciente}
-      />
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
       </div>
-      <Toaster position="bottom-center" richColors closeButton  />
+      <Toaster position="bottom-center" richColors closeButton />
     </div>
   )
 }
